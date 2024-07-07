5a7f527f1e136f2a4a9a04f97f0fa0c53a53c29f
JosueEC <josueev.cruz@gmail.com>
2024-07-06 18:39:21 -0600

    [FEAT](src): Add the NATS config

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ProductsController } from './products.controller';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import { PRODUCTS_SERVICE, envs } from 'src/config';
+import { NatsModule } from 'src/transports/nats.module';
 
 @Module({
   controllers: [ProductsController],
   providers: [],
   /**
+   * ? Registro simple de microservicio
    * El registro de microservicios se hace en el array de imports.
    * Esto puede hacerse en cualquier parte mientras el objeto
    * se agregue al array, en este caso se esta agregando en el
@@ -18,22 +18,42 @@ import { PRODUCTS_SERVICE, envs } from 'src/config';
    * también hay que usar el mismo método de transporte que
    * fue definido en el respectivo microservicio
    */
+  /**
+   * ? Centralización del código del transporter de comunicación
+   * Ahora, vamos a centralizar el código de configuración para el
+   * protocolo de comunicación entre microservicios
+   */
   imports: [
-    ClientsModule.register([
-      {
-        name: PRODUCTS_SERVICE,
-        transport: Transport.TCP,
-        /**
-         * Las options son los parámetros de conexión con el microservicio
-         * en este caso definimos el host y el puerto donde puede buscar al
-         * microservicio para hacer peticiones
-         */
-        options: {
-          host: envs.productsMicroserviceHost,
-          port: envs.productsMicroservicePort,
-        },
-      },
-    ]),
+    // ClientsModule.register([
+    /**
+     * ? Cambio de protocolo de comunicación de TCP a NATS
+     * Dado que ahora el microservicio de products se comunica
+     * a traves de NATS, es necesario también cambiar la configuración
+     * de comunicación del gateway para ese servicio, quedando de esta
+     * forma:
+     */
+    // {
+    //   name: PRODUCTS_SERVICE,
+    //   transport: Transport.TCP,
+    /**
+     * Las options son los parámetros de conexión con el microservicio
+     * en este caso definimos el host y el puerto donde puede buscar al
+     * microservicio para hacer peticiones
+     */
+    //   options: {
+    //     host: envs.productsMicroserviceHost,
+    //     port: envs.productsMicroservicePort,
+    //   },
+    // },
+    //   {
+    //     name: NATS_SERVICE,
+    //     transport: Transport.NATS,
+    //     options: {
+    //       servers: envs.natsServers,
+    //     },
+    //   },
+    // ]),
+    NatsModule,
   ],
 })
 export class ProductsModule {}
---
+++ src/transports/nats.module.ts
@@ -0,0 +1,35 @@
+import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { NATS_SERVICE, envs } from 'src/config';
+
+/**
+ * Ahora, todos los módulos van a poder importar la configuración
+ * de NATS para poder usarla, por ende añadimos la configuración al
+ * array de imports y asi mismo la exportamos en el array de
+ * exports
+ */
+@Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: NATS_SERVICE,
+        transport: Transport.NATS,
+        options: {
+          servers: envs.natsServers,
+        },
+      },
+    ]),
+  ],
+  exports: [
+    ClientsModule.register([
+      {
+        name: NATS_SERVICE,
+        transport: Transport.NATS,
+        options: {
+          servers: envs.natsServers,
+        },
+      },
+    ]),
+  ],
+})
+export class NatsModule {}
